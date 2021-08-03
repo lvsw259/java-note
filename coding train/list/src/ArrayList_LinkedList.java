c1c46d37eb1a796871038f86b21939c87a027c01
lvsw259 <53414159+lvsw259@users.noreply.github.com>
2021-08-03 12:27:47 +0800

    Add files via upload

---
+++ coding train/list/src/ArrayList_LinkedList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayList_LinkedList {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new LinkedList<>();
+        list1.add("break a leg");
+
+
+    }
+}
